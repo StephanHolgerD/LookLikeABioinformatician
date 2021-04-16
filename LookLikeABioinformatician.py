30cc3661dd153cd109c01c048cc44a46a5dffcd0
StephanHolgerD <stephan.drukewitz@uni-leipzig.de>
2021-04-16 20:53:54 +0200

    colors

--- LookLikeABioinformatician.py
+++ LookLikeABioinformatician.py
@@ -5,16 +5,15 @@ from time import sleep
 size=shutil.get_terminal_size((80, 20))
 
 
-
 #green \u001b[32m
 #print(dir(size))
 #\u001b[33m
 #blue \u001b[34m
 randHighlight=[0]*50+[1]*5
 print('\n')
-nucs=["\u001b[31mA\u001b[0m", "\u001b[33mC\u001b[0m", "\u001b[34mT\u001b[0m" , "\u001b[32mG\u001b[0m"]
+nucs=["\u001b[31mA\u001b[1m", "\u001b[33mC\u001b[1m", "\u001b[34mT\u001b[1m" , "\u001b[32mG\u001b[1m"]
 
-nucsH=["\u001b[47;1m\u001b[31mA\u001b[0m", "\u001b[47;1m\u001b[32mC\u001b[0m", "\u001b[47;1m\u001b[34mT\u001b[0m" , "\u001b[47;1m\u001b[32mG\u001b[0m"]+75*["\u001b[31mA\u001b[0m", "\u001b[32mC\u001b[0m", "\u001b[34mT\u001b[0m" , "\u001b[32mG\u001b[0m"]
+nucsH=["\u001b[47;1m\u001b[31mA\u001b[0m", "\u001b[47;1m\u001b[33mC\u001b[0m", "\u001b[47;1m\u001b[34mT\u001b[0m" , "\u001b[47;1m\u001b[32mG\u001b[0m"]+75*["\u001b[31mA\u001b[1m", "\u001b[33mC\u001b[1m", "\u001b[34mT\u001b[1m" , "\u001b[32mG\u001b[1m"]
 
 while True:
     if size != shutil.get_terminal_size((80, 20)):
